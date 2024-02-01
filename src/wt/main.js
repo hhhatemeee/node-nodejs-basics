529acacd991f02fe64265fd4902bf27582854480
ABiryulin <abiryulin@mmtr.ru>
2024-02-01 22:35:32 +0300

    Solved wt main task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,11 +1,35 @@
 import { Worker } from 'node:worker_threads'
+import os from 'node:os'
 import { fileURLToPath } from 'url';
 import path from 'path';
+
+const RESULT_STATUS = {
+    ERROR: 'error',
+    RESOLVED: 'resolved'
+}
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const performCalculations = async () => {
-    const worker = new Worker(path.join(__dirname, 'worker.js'), { workerData: Math.round(Math.random() * 10) })
+    const cpuCount = os.cpus().length
+    const result = []
+
+    for (let i = 0; i < cpuCount; i++) {
+        const worker = new Worker(path.join(__dirname, 'worker.js'), { workerData: i + 10 })
+
+        worker.on('message', msg => {
+            result[i] = ({ status: RESULT_STATUS.RESOLVED, data: msg })
+
+            if (result.length === cpuCount) {
+                console.log(result);
+            }
+        })
+
+
+        worker.on('error', () => {
+            result[i] = ({ status: RESULT_STATUS.ERROR, data: null })
+        })
+    }
 };
 
 await performCalculations();
